a9f1c308de0c2b4571f1b7b98a858d0fabff13f4
Max <96389721+TetherIO@users.noreply.github.com>
2023-05-27 13:38:48 -0700

    added comment "plan" for BinaryTree.h

--- BinaryTree.h
+++ BinaryTree.h
@@ -6,6 +6,13 @@
 
 using std::string;
 
+// Node class for BinaryTree. Each Node contains a key, and pointers to left and right children.
+
+    // Constructor for Node, takes a string key and initializes left and right children pointers to NULL.
+
+// BinaryTree class for handling collisions in Hashtable. Each bucket in the Hashtable will contain a BinaryTree.
+
+      // Constructor for BinaryTree, initializes root to NULL.
 
 
 #endif //CS260_SPRING_23_ASSIGNMENT_7_BINARYTREE_H
