988177e821c1a138af8e8ed52c98d9e8ec767361
Max <96389721+TetherIO@users.noreply.github.com>
2023-05-27 16:40:14 -0700

    added node struct

--- BinaryTree.h
+++ BinaryTree.h
@@ -5,14 +5,22 @@
 #include <string>
 
 using std::string;
-
 // Node class for BinaryTree. Each Node contains a key, and pointers to left and right children.
+struct Node {
+    string key;
+    Node *left;
+    Node *right;
 
-    // Constructor for Node, takes a string key and initializes left and right children pointers to NULL.
+    // constructor for Node, takes a string key and initializes left and right children pointers to nullptr.
+    Node(string key) : key(key), left(nullptr), right(nullptr) {}
+};
 
-// BinaryTree class for handling collisions in Hashtable. Each bucket in the Hashtable will contain a BinaryTree.
+//binaryTree class for handling collisions in Hashtable. Each bucket in the Hashtable will contain a BinaryTree.
+class BinaryTree {
+public:
+    Node *root; // pointer to the root node of the BinaryTree
 
-      // Constructor for BinaryTree, initializes root to NULL.
+      //constructor for BinaryTree, initializes root to nullptr
 
 
 #endif //CS260_SPRING_23_ASSIGNMENT_7_BINARYTREE_H
