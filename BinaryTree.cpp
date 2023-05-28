95be0a69e0ab5674987dfef74b373211648e1160
Max <96389721+TetherIO@users.noreply.github.com>
2023-05-28 12:25:49 -0700

    Update BinaryTree.cpp

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -5,6 +5,12 @@ Node *BinaryTree::insertNode(Node *root, string key) {
     if (root == nullptr) {  // if tree is empty, assign a new node address to root
         root = new Node(key);
       //left right here
+    } else if (key <= root->key) { //if incoming key is less than the root node key, insert into left branch
+        root->left = insertNode(root->left, key);
+    } else { // else, insert into right 
+        root->right = insertNode(root->right, key);
+    }
+    return root;
 }
 
 //insert a key into the binary tree
