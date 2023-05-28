2b8b0dd5dde461b58ceb7e3001f64e97925f4d42
Max <96389721+TetherIO@users.noreply.github.com>
2023-05-28 12:58:13 -0700

    finished simple BinaryTree.cpp

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -20,7 +20,17 @@ void BinaryTree::insert(string key) {
 
 
 // helper function to find a node in the binary tree
-
+bool BinaryTree::findNode(Node *root, string key) {
+    if (root == nullptr) { //if tree/subtree is empty, return false
+        return false;
+    } else if(root->key == key) { // if key, then true is returned
+        return true;
+    } else if (key < root->key) { // if the incoming key is less than the root's, search in the left branch
+        return findNode(root->left, key);
+    } else { // eslse, search in  right 
+        return findNode(root->right, key);
+    }
+}
 
 // search a key in the binary tree
 bool BinaryTree::find(string key) {
