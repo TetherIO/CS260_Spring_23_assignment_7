3fe2fb6eacfa4436fda79aca6ce8ac21aea92e51
Max <96389721+TetherIO@users.noreply.github.com>
2023-05-28 11:12:39 -0700

    added structure to BinaryTree.cpp

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -1,5 +1,17 @@
-//
-// Created by user7 on 5/24/2023.
-//
-
 #include "BinaryTree.h"
+
+
+
+// helper function to insert a node into the binary tree
+
+
+//insert a key into the binary tree
+
+
+
+// helper function to find a node in the binary tree
+
+
+// search a key in the binary tree
+
+
