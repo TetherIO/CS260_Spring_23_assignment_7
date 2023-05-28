88e19a0fbc1902b868d0401ca13dc6bbe292859a
Max <96389721+TetherIO@users.noreply.github.com>
2023-05-28 11:32:07 -0700

    added a few functions to BinaryTree.cpp

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -1,17 +1,23 @@
 #include "BinaryTree.h"
 
-
-
 // helper function to insert a node into the binary tree
-
+Node *BinaryTree::insertNode(Node *root, string key) {
+    if (root == nullptr) {  // if tree is empty, assign a new node address to root
+        root = new Node(key);
+      //left right here
+}
 
 //insert a key into the binary tree
-
+void BinaryTree::insert(string key) {
+    root = insertNode(root, key);
+}
 
 
 // helper function to find a node in the binary tree
 
 
 // search a key in the binary tree
-
+bool BinaryTree::find(string key) {
+    return findNode(root, key);
+}
 
