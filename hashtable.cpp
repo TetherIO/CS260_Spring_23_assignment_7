694cf744aeaef56935b2eeaba0190e02e79c71c3
Max <96389721+TetherIO@users.noreply.github.com>
2023-05-29 11:15:34 -0700

    wrote ht constructor hashtable.cpp

--- hashtable.cpp
+++ hashtable.cpp
@@ -1,1 +1,25 @@
+#include "hashtable.h"
 
+// Initialize hashtable with a certain capacity -- buckets will be a binary trees.
+Hashtable::Hashtable(int initialSize) {
+    size = 0;
+    capacity = initialSize;
+    buckets = new BinaryTree[capacity]; // allocate memory for BT array of size 'capacity'
+}
+
+// Destructor for allocated memory
+
+
+// Compute a hash value for the input key (hash function)
+
+
+// Add a key to the hash table
+
+
+// check if a key exists in hash table
+
+
+//=get the number of keys currently stored
+
+
+// get the current capacity
