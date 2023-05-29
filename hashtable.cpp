fd1d5f665b3ccee003cee327332e2d2f60854457
Max <96389721+TetherIO@users.noreply.github.com>
2023-05-29 14:33:11 -0700

    tested using gcc suite, working hashtable.cpp

--- hashtable.cpp
+++ hashtable.cpp
@@ -1,6 +1,6 @@
 #include "hashtable.h"
 
-// Initialize hashtable with a certain capacity -- buckets will be a binary trees.
+// Initialize hashtable with a givven capacity -- buckets will be a binary trees.
 Hashtable::Hashtable(int initialSize) {
     size = 0;
     capacity = initialSize;
@@ -22,10 +22,17 @@ int Hashtable::hash(string key) {
 }
 
 // Add a key to the hash table
-
+void Hashtable::add(string newKey) {
+    int index = hash(newKey); // computation of hash value to determine the index (bucket) for this key
+    buckets[index].insert(newKey); // Insert newKey into the BT at the computed bucket
+    size++;
+}
 
 // check if a key exists in hash table
-
+bool Hashtable::find(string key) {
+    int index = hash(key); //  hash value to determine the index (bucket) for this key
+    return buckets[index].find(key); // Check if the key exists at the bucket
+}
 
 //=get the number of keys currently stored
 int Hashtable::getSize() {
